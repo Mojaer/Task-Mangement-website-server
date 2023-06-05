2f2cc7a90dc707765c78ee09fc4b7e5f63d5a295
Mojaer Ahmed <mojaermohi@gmail.com>
2023-06-05 20:30:41 +0600

    finish

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function MongoRun() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const database = client.db("taskDB");
         const taskCollection = database.collection("tasks");
